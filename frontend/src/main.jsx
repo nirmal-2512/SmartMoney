9cc64f498b1d7569da49c9e8cb3bc8be33e74dd0
nirmal-2512 <nirmalpatidar2533@gmail.com>
2026-06-06 21:56:40 +0530

    Deploy Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
+import { inject } from "@vercel/analytics";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,6 +15,7 @@ const queryClient = new QueryClient({
   },
 });
 
+inject(); // add this before ReactDOM.render
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
